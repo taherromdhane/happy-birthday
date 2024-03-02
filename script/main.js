06b07870100dd35ebcb3841871923dc6037eb101
taherromdhane <romdhane.attaher@gmail.com>
2024-03-02 17:36:01 +0100

    change name

--- script/main.js
+++ script/main.js
@@ -135,7 +135,7 @@ const animationTimeline = () => {
     .to(".idea-3 strong", 0.5, {
       scale: 1.2,
       x: 10,
-      backgroundColor: "rgb(21, 161, 237)",
+      backgroundColor: "rgb(144, 0, 0)",
       color: "#fff"
     })
     .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
